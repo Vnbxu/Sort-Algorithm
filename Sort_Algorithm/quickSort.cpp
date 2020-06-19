d8a94d652423dfcade5765c47106e54481ab52c8
Vnbxu <54344296+Vnbxu@users.noreply.github.com>
2020-06-19 21:44:19 +0800

    update

--- Sort_Algorithm/quickSort.cpp
+++ Sort_Algorithm/quickSort.cpp
@@ -1,13 +1,17 @@
 template<typename T>
 void quickSort(vector<T>& nums, int start, int end) {
 	if (start >= end)	return;
-	int i = start, j = end;
+	int i = start,j = end;
 	T base = nums[start];
 	while (i < j) {
-		while (i < j && nums[j] >= base)	j--;
-		if (i < j)	nums[i++] = nums[j];
-		while (i < j && nums[i] <= base)	i++;
-		if (i < j)	nums[j--] = nums[i];
+		while (i < j && nums[j] >= base)	
+			j--;
+		if (i < j)
+			nums[i++] = nums[j];
+		while (i < j && nums[i] <= base)	
+			i++;
+		if (i < j)	
+			nums[j--] = nums[i];
 	}
 	nums[i] = base;
 	quickSort(nums, start, i - 1);
